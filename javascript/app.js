356d4f01a557dd95df47a647ddac38fa79e7ff5f
Robin <mehmet.ucar@imediapro.co.uk>
2020-10-03 17:20:19 +0100

    change backround color to added color by user

--- javascript/app.js
+++ javascript/app.js
@@ -1,28 +1,38 @@
 // background colors array
 
-const colors = ["red", "blue", "green", "gray", "purple"];
+const colors = ['red', 'blue', 'green', 'gray', 'purple'];
 
-//reach button
+//reach buttons
 
-const buttonElement = document.querySelector("#randomColorButton");
+const buttonElement = document.querySelector('#randomColorButton');
+
+const addColorButton = document.querySelector('#setColorButton');
 
 //event listeners
 
-buttonElement.addEventListener("click", changeColors);
+buttonElement.addEventListener('click', changeColors);
 
+addColorButton.addEventListener('click', addColor);
 
 //change color function
 
 function changeColors() {
   //get colors randomly
 
-  const randomIndex = Math.floor((Math.random() * colors.length));
+  const randomIndex = Math.floor(Math.random() * colors.length);
 
   // get color colorName
 
-  document.querySelector("#colorName").textContent = colors[randomIndex]
+  document.querySelector('#colorName').textContent = colors[randomIndex];
 
   //change colors randomly
 
-  document.querySelector("body").style.backgroundColor = colors[randomIndex];
+  document.querySelector('body').style.backgroundColor = colors[randomIndex];
+}
+
+//add colors
+
+function addColor() {
+  const userInputValue = document.querySelector('.colorInput').value;
+  document.querySelector('body').style.backgroundColor = userInputValue;
 }
